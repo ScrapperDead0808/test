a95db07c35d8ce968c696c896ac983f7f9a7f86d
ScrapperDead0808 <113934727+ScrapperDead0808@users.noreply.github.com>
2023-03-03 16:13:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_his(namexx):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {names}')  # Press Ctrl+F8 to toggle the breakpoint.
+ 
+    print(f'Hi, {namesttt}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     print_his('PyCharms')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
